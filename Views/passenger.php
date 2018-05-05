cd41e33b9448b0bc79e86259d6d40b9a550387e4
JeanVanneste <jean.vanneste@student.vinci.be>
2018-05-05 15:30:01 +0200

    Create page passenger.php

---
+++ Views/passenger.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Passage</title>
+</head>
+<body>
+    <h1>
+        Nouveau passager
+    </h1>
+    <form method="post">
+        <table>
+            <tr>
+                <th>Nom</th>
+                <th><input type="text" name="nom"></th>
+            </tr>
+
+            <tr>
+                <th>Prénom</th>
+                <th><input type="text" name="prenom"></th>
+            </tr>
+            <tr>
+                <th>Age</th>
+                <th><input type="text" name="age"></th> 
+            </tr>
+        </table>
+        <button type="submit">Etape suivante</button>
+        <button type="reset">Retour à la page précédente</button>
+        <button type="">Annuler la réservation</button>
+
+</form>
+</body>
+</html>
