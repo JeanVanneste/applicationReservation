b42b01ba0b037d550671284c60de823b4f647a46
JeanVanneste <jean.vanneste@student.vinci.be>
2018-05-05 15:02:04 +0200

    Complétion passenger.php

---
+++ Models/passenger.php
@@ -0,0 +1,27 @@
+<?php
+
+class Passenger
+{
+    public $id;
+    public $firstname;
+    public $lastname;
+    public $age;
+    public $fligthId;
+
+    function __construct($lastId, $firstname, $lastname, $age, $fligthId)
+    {
+        $this->id = $lastId + 1;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->age = $age;
+        $this->fligthId = $fligthId;
+    }
+
+    function getPassengerPrice()
+    {
+        if ($this -> age <= 12){return 10;}
+        else {return 15;}
+    }
+}
+
+?>
