f0b94ed38e8b511a98465a9a0064671730e4de39
JeanVanneste <17242@ecam.be>
2018-07-25 15:48:14 +0200

    ajout .gitignore

--- test/testModels.php
+++ test/testModels.php
@@ -3,13 +3,13 @@
 require_once '../Models/reservation.php';
 require_once '../Models/passenger.php';
 
-$reservation = new Reservation(1, "Bruxelles", 3, false);
+$reservation = new Reservation(1, "Bruxelles", 3, true);
 $reservation->addPassenger(0, "Basile", "Vanneste", 21);
 $reservation->addPassenger(1, "Jean", "Vanneste", 23);
 
 $reservation->printReservation();
 
-echo($reservation->getTotalPrice());
+echo("Total : ".$reservation->getTotalPrice(). "€");
 
 /*
 $passenger = new Passenger(0, "Jean", "Vanneste", 23, 1);
