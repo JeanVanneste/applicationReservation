abd8385d1ea011d8b9b221165b89bf2e3061426b
JeanVanneste <jean.vanneste@student.vinci.be>
2018-07-26 18:40:56 +0200

    Cancel page complete

--- Views/cancel.php
+++ Views/cancel.php
@@ -1,1 +1,12 @@
-Annulation
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Réservation annulé</title>
+</head>
+<body>
+<h1>Réservation annulée</h1>
+<form action="index.php">
+	<button type="submit">Retour à la page d'accueil</button>
+</form>
+</body>
+</html>
